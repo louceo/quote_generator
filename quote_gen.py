93f61988af6321d6b7635a6760090f2fa2a54666
Artem K <109920720+louceo@users.noreply.github.com>
2025-12-18 21:16:54 +0300

    Update quote_gen.py

--- quote_gen.py
+++ quote_gen.py
@@ -8,22 +8,6 @@ api_key = os.getenv('API_KEY')
 URL = "https://api.api-ninjas.com/v1/quotes"
 headers = {"X-Api-Key": api_key}
 
-# def get_quote():
-#     response = requests.get(URL, headers=headers)
-#     data = response.json()
-#     return data
-
-# def load_quote():
-#     while True:
-#         data = get_quote()
-#         if len(data[0]["quote"]) < 120:    
-#             break
-#     return {
-#         "quote": data[0]["quote"],
-#         "author": data[0]["author"],
-#         "quote length": len(data[0]["quote"])
-#     }
-
 def get_quote():
     response = requests.get(URL, headers=headers)
     data = response.json()
